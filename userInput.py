9135f99b7c3d0ffa3b5d57931a285798daf6e75a
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2024-10-15 21:21:35 +0600

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,23 @@
+#cm²(Hold ALT+Num Lk+0178)
+n=input("Enter any number :  ")
+print(n)
+#here the number is enter as a string variable
+
+number=int(input("Enter any number: "))#convert the number into integer
+number+=1
+print(number)
+
+#Exericise
+num=float(input("Enter any number : "))
+num2=float(input("Enter another number: "))
+sum=num+num2
+mul=num*num2
+sub=num-num2
+div=num/num2
+avg=(num+num2)/2
+
+print(f"The sum of the numbers are {sum}")
+print(f"The mutiplication of the numbers are {mul}")
+print(f"The substriction of the numbers  are {sub}")
+print(f"The division of the numbers are {div}")
+print(f"The average of the numbers age {avg}")
